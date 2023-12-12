2df9395c8b76189cca7304c94445fd05f80bcaa0
Outtacosmos-ai <messghir4@gmail.com>
2023-12-12 15:31:50 +0100

    rm 2 files

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-wget -P .. https://raw.githubusercontent.com/Outtacosmos-ai/holbertonschool-low_level_programming/master/0x18-dynamic_libraries/libgiga.so
-export LD_PRELOAD="$PWD/../libgiga.so"
