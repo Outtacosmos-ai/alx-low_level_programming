4ec09972cff8f9804e34fb028f26812a0dc31b39
Outtacosmos-ai <messghir4@gmail.com>
2023-12-12 16:59:56 +0100

    101-make_me_win.sh

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-wget -P /tmp http://github.com/Outtacosmos-ai/alx-low_level_programming/raw/master/0x18-dynamic_libraries/libgiga.ss export LD_PRELOAD=/tmp/libgiga.so
+wget -q -O /tmp/gigalib.so "https://raw.githubusercontent.com/Outtacosmos-ai/alx-low_level_programming/master/0x18-dynamic_libraries/gigalib.so"
+export LD_PRELOAD=/tmp/gigalib.so
