7a4dca4167af338b64fdcc9ce322a54946a87ef6
Outtacosmos-ai <messghir4@gmail.com>
2024-12-18 16:26:34 +0100

    All at once

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -8,31 +8,27 @@ def island_perimeter(grid):
     Calculate the perimeter of the island described in the grid.
 
     Args:
-    grid (List[List[int]]): A list of list of integers where:
-        - 0 represents a water zone
-        - 1 represents a land zone
+    grid (List[List[int]]): A list of list of integers representing the island.
 
     Returns:
-    int: The perimeter of the island
-
-    Note:
-    - One cell is a square with side length 1
-    - Grid cells are connected horizontally/vertically (not diagonally)
-    - Grid is rectangular, width and height don't exceed 100
-    - Grid is completely surrounded by water, and there is one island (or nothing)
-    - The island doesn't have "lakes" (water inside that isn't connected to the water around the island)
+    int: The perimeter of the island.
     """
     perimeter = 0
     rows = len(grid)
-    cols = len(grid[0]) if rows else 0
+    cols = len(grid[0])
 
     for i in range(rows):
         for j in range(cols):
             if grid[i][j] == 1:
                 perimeter += 4
+
                 if i > 0 and grid[i-1][j] == 1:
-                    perimeter -= 2
+                    perimeter -= 1
+                if i < rows-1 and grid[i+1][j] == 1:
+                    perimeter -= 1
                 if j > 0 and grid[i][j-1] == 1:
-                    perimeter -= 2
+                    perimeter -= 1
+                if j < cols-1 and grid[i][j+1] == 1:
+                    perimeter -= 1
 
     return perimeter
